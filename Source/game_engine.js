475372e55de85e6691886c02dbbc677781ccd5f0
Iván Martín <43232261+Ivan-Martin@users.noreply.github.com>
2019-09-25 09:31:12 +0200

    Añadidas funcionalidades básicas al motor de juego
    Se han añadido funcionalidades básicas al motor de juego, como la utilización del canvas y el print de imágenes

--- Source/game_engine.js
+++ Source/game_engine.js
@@ -3,7 +3,38 @@
 In this file, all game engine functions are located.
 
 */
+function create () {
+    /*
+    Function CREATE:
+    - Creates a canvas, size 640*640 in the document
+    - Appends the canvas to the document
+    */
+    let canvas = document.createElement("CANVAS");
+    canvas.id = "game";
+    canvas.setAttribute("width",640);
+    canvas.setAttribute("height",640);
+    canvas.setAttribute('style', "position: absolute;  left: 30%; top: 10%; border:2px");
+    document.body.appendChild(canvas);
 
-function drawImage() {
-    
+}
+
+function update(myboard) {
+    /*
+    FUNCTION UPDATE:
+    - Clears canvas context.
+    - Calls update of every active object in the game
+    - Prints new canvas with updated information of each object
+    It's called 50 times per second.
+    */
+    let context = document.getElementById("game").getContext("2d");
+    context.clearRect(0, 0, 640, 640);
+
+    myboard.elements.array.forEach(element => {
+        element.update();
+    });
+}
+
+function printImage(image, ){
+    let context = document.getElementById("game").getContext("2d");
+
 }
--- Source/main_game.js
+++ Source/main_game.js
@@ -4,35 +4,35 @@ MAIN GAME JAVASCRIPT. This file calls all functions and contains the MAIN GAME.
 
 class Game {
 
-    constructor () {
-        /*
-        Constructor of class GAME:
-        - Creates a canvas, size 640*640 in the document
-        - Appends the canvas to the document
-        */
-        var canvas = document.createElement("CANVAS");
-        canvas.id = "game";
-        canvas.setAttribute("width",640);
-        canvas.setAttribute("height",640);
-        canvas.style="border:1px solid #000000";
-        canvas.setAttribute('style', "position: absolute;  left: 30%; top: 10%; border:2px");
-        document.body.appendChild(canvas);
-
-        
-    }
-
     create (x, y) {
         /*
-        FUNCTION create: Creates a world in the canvas. Prints it.
+        FUNCTION create:
         */
         this.world = new Array(x);
         for (var i = 0; i < x; i++){
             this.world[i] = new Array(y);
         }
+        //Attribute "world" of Game:
+        //Consists of an array with the information of all the board. 
+
+        this.elements = new Array(0);
+        //Attribute "elements" of Game:
+        //Consists of an array with all the active elements in scene.
+        //Used for rendering and other options.
+    }
+
+    addElement(element){
+        /*
+        FUNCTION addElement:
+        - Adds an element to the world
+        */
+        this.elements.push(element);
     }
 
 
 }
 
 var unnamed = new Game();
-unnamed.create(20, 10);
+create();
+unnamed.create(10, 10);
+setInterval(function () {update(unnamed)}, 20);
